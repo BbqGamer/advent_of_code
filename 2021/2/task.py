8d6ebccee1d78685a05a1dd8cbe6b985c9888c54
adam <korba.adam@gmail.com>
2021-12-02 08:12:11 +0100

    Add day 2, year 2021

---
+++ 2021/2/task.py
@@ -0,0 +1,26 @@
+#Part one
+with open("input") as f:
+    horizontal, depth = 0, 0
+    for line in f:
+        command, value = line.split()
+        if command == "forward":
+            horizontal += int(value)
+        if command == "down":
+            depth += int(value)
+        if command == "up":
+            depth -= int(value)
+    print(horizontal * depth)
+
+#Part two
+with open("input") as f:
+    horizontal, depth, aim = 0, 0, 0
+    for line in f:
+        command, value = line.split()
+        if command == "forward":
+            horizontal += int(value)
+            depth += int(value) * aim
+        if command == "down":
+            aim += int(value)
+        if command == "up":
+            aim -= int(value)
+    print(horizontal * depth)
