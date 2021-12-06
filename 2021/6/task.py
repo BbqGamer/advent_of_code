0b9ec55fdab2f3655a53a802db2a08b4a2a28557
adam <korba.adam@gmail.com>
2021-12-06 14:05:00 +0100

    modify day 6

--- 2021/6/task.py
+++ 2021/6/task.py
@@ -1,18 +1,16 @@
 with open("input") as f:
     inputFish = [int(x) for x in f.readline().rstrip().split(",")]
-    fish = {i:inputFish.count(i) for i in range(9)}
+    fish = [inputFish.count(i) for i in range(9)]
     zeros = 0
     for i in range(256):
-        newFish = {i:0 for i in range(9)}
-        for k in range(9):
-            if k == 0:
-                zeros = fish[k]
-                newFish[k] = 0
-            else:
+        newFish = [0 for i in range(9)]
+        zeros = fish[0]
+        newFish[0] = 0
+        for k in range(1,9):
                 newFish[k-1] = fish[k]
         newFish[6] += zeros
         newFish[8] += zeros
         fish = newFish
         if i == 79:
-            print("Part 1: ", sum(fish.values()))    
-    print("Part 2: ", sum(fish.values()))
+            print("Part 1: ", sum(fish))    
+    print("Part 2: ", sum(fish))
